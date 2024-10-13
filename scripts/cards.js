b8f998743ddf51f22bc908ec1c23f33db54e6ab5
Nataliia Kotlomina <kotlomina.nataliia@yandex.ru>
2024-10-13 21:04:55 +0300

    Выполнен проект, первая сдача

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      altDescription: "Фотография горного хребта, покрытого зеленью",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      altDescription: "Фотография небольшого горного озера зимой",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      altDescription: "Фотография монолитных многоэтажек вечером, в некоторых окнах зажжён свет",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      altDescription: "Фотография горного плато, покрытого редкой зеленью и мхами",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      altDescription: "Фотография уходящих вдаль железнодорожных путей посреди леса",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      altDescription: "Фотография скалистого берега покрытого льдом озера зимой",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,33 @@
 
 // @todo: Функция создания карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard(name, link, altDescription, deleteCard) {
+
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = altDescription;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
+};
+
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+  const cardToDelete = evt.target.closest('.places__item');
+  cardToDelete.remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((element) => {
+  const placesList = document.querySelector('.places__list');
+  const card = createCard(element.name, element.link, element.altDescription, deleteCard);
+  placesList.append(card);
+});
