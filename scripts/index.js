217fb18f6ae4a8235b5421e1846f2c418c2895f1
Nataliia Kotlomina <kotlomina.nataliia@yandex.ru>
2024-10-14 07:58:33 +0300

    Внесены поправки согласно замечаниям

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@
 
 // @todo: Функция создания карточки
 
+const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
 function createCard(name, link, altDescription, deleteCard) {
@@ -11,8 +12,9 @@ function createCard(name, link, altDescription, deleteCard) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
   cardElement.querySelector('.card__title').textContent = name;
-  cardElement.querySelector('.card__image').src = link;
-  cardElement.querySelector('.card__image').alt = altDescription;
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = link;
+  cardImage.alt = altDescription;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', deleteCard);
@@ -30,7 +32,6 @@ function deleteCard(evt) {
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((element) => {
-  const placesList = document.querySelector('.places__list');
   const card = createCard(element.name, element.link, element.altDescription, deleteCard);
   placesList.append(card);
 });
